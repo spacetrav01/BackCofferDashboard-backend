69d68f64a7c2c3fc2d9358cb145ea4c397ca18f4
Om Shahane <71377782+shahane806@users.noreply.github.com>
2024-03-08 16:03:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cors = require("cors");
 const router = require("./Router/Router");
 const { default: mongoose } = require("mongoose");
 const app = express();
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
+
 app.use(express.json());
 app.use(cors());
 app.use(router);

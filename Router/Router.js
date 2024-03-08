ffe0092e741074f85983f9e86cb068808318607e
Om Shahane <71377782+shahane806@users.noreply.github.com>
2024-03-08 16:40:37 +0530

    Update Router.js

--- Router/Router.js
+++ Router/Router.js
@@ -1,4 +1,5 @@
 const express = require("express");
 const router = express.Router();
 const fetchDataController = require("../Controller/fetchDataController");
-module.exports = router.get("/", fetchDataController);
+module.exports = router.get("/fetch", fetchDataController);
+module.exports = router.get("/", async(req,res)=>{await res.status(200).send("Hello Data")};

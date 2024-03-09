bbc02e6515644cdd56431a4684a75af744ef1b95
Om Shahane <71377782+shahane806@users.noreply.github.com>
2024-03-09 10:21:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ app.use(cors());
 app.use(router);
 //connecting to mongodb
 const url = process.env.REACT_APP_MONGODB_CONNECTION_URL;
+const port = process.env.PORT || 3000;
 mongoose.connect(url).then(()=>{
   console.log("We are successfully connected to MongoDb")
 })
-app.listen(3000, () => {
+app.listen(port,"0.0.0.0", () => {
   console.log("running");
 });
